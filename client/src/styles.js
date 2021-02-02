2ec9712acb2accbfd69863659d921ff670d2ef71
Luis Abellan <luis.abellan.dev@gmail.com>
2021-02-02 20:22:51 +0100

    typo
    textPageWitdh --> textPageWidth

--- client/src/styles.js
+++ client/src/styles.js
@@ -10,7 +10,7 @@ export const unit = 8;
 export const widths = {
   largePageWidth: 1600,
   regularPageWidth: 1100,
-  textPageWitdh: 800,
+  textPageWidth: 800,
 };
 export const colors = {
   primary: SKColors.indigo.base,
