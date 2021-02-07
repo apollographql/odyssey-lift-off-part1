0b7a213dec78a737c5894f8fa8f7825c6813b890
Luis Abellan <luis.abellan.dev@gmail.com>
2021-02-07 18:42:25 +0100

    schema created

---
+++ server/src/schema.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    "query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "the track's title"
+    title: String!
+    "the track's author"
+    author: Author!
+    "the track's main illustration"
+    thumbnail: String
+    "the track's approximate length to complete in minutes"
+    length: Int
+    "the number of modules this track contains"
+    modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "the author's name"
+    name: String!
+    "the author's photo"
+    photo: String
+  }
+`
+
+module.exports = typeDefs
