75e872865eb26d62d29b780372ebf85c38744a8f
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-07-15 14:43:51 -0600

    update react-markdown to v6

--- client/src/components/md-content.js
+++ client/src/components/md-content.js
@@ -7,7 +7,7 @@ import ReactMarkdown from 'react-markdown';
  * Markdown component is a simple style wrapper for markdown content used across our app
  */
 const MarkDown = ({ content }) => {
-  return <StyledMarkdown source={content} />;
+  return <StyledMarkdown children={content} />;
 };
 
 export default MarkDown;
