79b12486784a2c1f698f8c1a22eaa2e62a9cde74
Lisa <lb.broadhead@gmail.com>
2022-09-23 08:52:36 -0600

    christmas page

---
+++ client/src/christmas.js
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Merry Christmas!</title>
+</head>
+<body>
+    <main>
+        <h1>Merry Christmas</h1>
+        <p>Christmas Christmas Christmas Christmas!</p>
+    </main>
+</body>
+</html>
