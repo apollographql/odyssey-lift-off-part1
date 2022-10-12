38d241253f7be9a0a9c0457c6db01cd241704193
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-10-12 14:05:19 -0400

    code changes to upgrade to Apollo Server 4

--- final/server/src/index.js
+++ final/server/src/index.js
@@ -1,9 +1,13 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { addMocksToSchema } = require('@graphql-tools/mock');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+
 const typeDefs = require('./schema');
 
 const mocks = {
   Query: () => ({
-    tracksForHome: () => [...new Array(9)],
+    tracksForHome: () => [...new Array(6)],
   }),
   Track: () => ({
     id: () => 'track_01',
@@ -11,26 +15,28 @@ const mocks = {
     author: () => {
       return {
         name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+        photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
       };
     },
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
     length: () => 1210,
     modulesCount: () => 6,
   }),
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  mocks,
-});
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
+  const { url } = await startStandaloneServer(server);
 
-server.listen().then(() => {
   console.log(`
-    🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at http://localhost:4000
-`);
-});
+      🚀  Server is running
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
--- final/server/src/schema.js
+++ final/server/src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+const gql = require('graphql-tag');
 
 const typeDefs = gql`
   type Query {
