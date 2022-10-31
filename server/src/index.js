41c38fd6b37cedde25aaa6c3da72a4e3bfe128c6
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-10-31 21:03:54 +0100

    Switched to module system

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,2 @@
-//TODO
+import {ApolloServer} from 'apollo-server';
+import typeDefs from './schema.js';
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+import { gql } from 'apollo-server';
 
 const typeDefs = gql`
 
@@ -25,4 +25,4 @@ const typeDefs = gql`
   }
 `;
 
-module.exports = typeDefs;
+export default typeDefs;
