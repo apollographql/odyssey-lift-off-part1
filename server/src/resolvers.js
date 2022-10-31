599de95ceadbda0806a383957ada9e789b2d988d
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 00:48:15 +0100

    Added Track resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,8 +1,14 @@
 const resolvers = {
     Query: {
-        // returns an array of Tracks that will be used to populate
-        // the homepage grid of our web client
-        tracksForHome: (parent, args, context, info) => { },
+        // get all tracks, will be used to populate the homepage grid of our web client
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
     }
 };
 
