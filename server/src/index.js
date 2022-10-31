ef65edb987f5df0a4c88cc7492c7031476ea6f1f
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-10-31 21:10:42 +0100

    Created and runnig Apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,19 @@
 import {ApolloServer} from 'apollo-server';
 import typeDefs from './schema.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const server = new ApolloServer({typeDefs});
+
+try {
+  await server.listen();
+  console.log(`
+      🚀  Server is running!
+      🔉  Listening on port ${PORT}
+      📭  Query at http://localhost:${PORT}
+    `);
+} catch (error) {
+  console.log('Something wrong!')
+}
