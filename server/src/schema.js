1f6192f8cd00d67ab08e3054f9787874ef4678e7
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-10-31 20:55:24 +0100

    Added a description to types

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  "A track is a group of Modules that teaches about a specific topic"
   type Track{
     id: ID!
     title: String!
@@ -9,10 +10,13 @@ const typeDefs = gql`
     lenght: Int
     modulesCount: Int
   }
+
+  "Author of a complete Track or a Module"
   type Author{
   id: ID!
   name: String!
   photo: String!
   }
 `;
+
 module.exports = typeDefs;
