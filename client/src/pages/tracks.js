52a8caa9e9d75e06742990e5a50f19940b433100
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-10-31 23:15:52 +0100

    Added TRACKS query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
