35b0799b918335b337c4fd75fccb3abdddebf38c
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 00:54:03 +0100

    Added resolvers and dataSources to ApolloServer options. Deleted mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,34 +1,20 @@
 import {ApolloServer} from 'apollo-server';
 import typeDefs from './schema.js';
+import resolvers from './resolvers.js'
+import TrackApi from './datasources/track-api.js'
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)]
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
-      };
-    },
-  })
-};
-
 const server = new ApolloServer({
   typeDefs,
-  mocks
+  resolvers,
+  dataSources: () => {
+    return {
+      trackApi: new TrackApi()
+    }
+  }
 });
 
 try {
