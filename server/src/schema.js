263fca239cbb4c494b058b732d8b8137587b527d
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 10:07:51 +0100

    Modified the schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -9,19 +9,31 @@ const typeDefs = gql`
 
   "A track is a group of Modules that teaches about a specific topic"
   type Track{
+    "The ID of track"
     id: ID!
+    "The track's title"
     title: String!
+    "The track's author"
     author: Author!
+    "The track's illustration to display in track card or track page detail"
     thumbnail: String
+    "The track's approximate length to complete, in minutes"
     length: Int
+    "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
   }
 
   "Author of a complete Track or a Module"
-  type Author{
-  id: ID!
-  name: String!
-  photo: String!
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture"
+    photo: String
   }
 `;
 
