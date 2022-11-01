43525e7ef39dcd519067f129ce8c5fdba80b9fb8
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 13:04:04 +0100

    Added mutation incrementTrackViews

--- server/src/schema.js
+++ server/src/schema.js
@@ -8,6 +8,21 @@ const typeDefs = gql`
     track(id: ID!): Track
   }
 
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse! 
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track{
     "The ID of track"
