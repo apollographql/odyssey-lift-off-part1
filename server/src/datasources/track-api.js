70de68f0bfa9e6df23eb7b477e88b96c3276bf7d
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 13:05:14 +0100

    Added method incrementTrackViews to data source class that using third part API

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -21,5 +21,9 @@ class TrackAPI extends RESTDataSource {
     getTrackModules(trackId) {
         return this.get(`track/${trackId}/modules`);
     }
+
+    incrementTrackViews(trackId) {
+        return this.patch(`track/${trackId}/numberOfViews`);
+    }
 }
 export default TrackAPI;
