74284783379746a527bce032399add3f4bbcc5af
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 13:42:49 +0100

    Added useMutation hook in the track-card

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -2,8 +2,22 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
-import {Link} from '@reach/router';
+import { Link } from '@reach/router';
+import { gql, useMutation } from '@apollo/client';
 
+const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`
 /**
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
@@ -11,8 +25,17 @@ import {Link} from '@reach/router';
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
 
+  const [incrementTrackViews, {loading, error, data}] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    onCompleted: data => {
+      console.log(data);
+    }
+  })
+
   return (
-    <CardContainer to={`/track/${id}`} >
+    <CardContainer
+      to={`/track/${id}`}
+      onClick={incrementTrackViews} >
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
