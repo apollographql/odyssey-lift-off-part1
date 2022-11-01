95aff16c79f1b9dfbfd983d06ea46ea808fa261b
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 13:17:25 +0100

    Added mutation resolve method

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,15 +4,28 @@ const resolvers = {
         tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackApi.getTracksForHome();
         },
-        track: (_, {id}, {dataSources}) => {
+        track: (_, { id }, { dataSources }) => {
             return dataSources.trackApi.getTrack(id);
         },
     },
+
+    Mutation: {
+        incrementTrackViews: async (_, { id }, { dataSources }) => {
+            const track = await dataSources.trackApi.incrementTrackViews(id);
+            return {
+                code: 200,
+                success: true,
+                message: `Successfully incremented number of views for track ${id}`,
+                track
+            };
+        }
+    },
+
     Track: {
         author: ({ authorId }, _, { dataSources }) => {
             return dataSources.trackApi.getAuthor(authorId);
         },
-        modules: ({id}, _, {dataSources}) => {
+        modules: ({ id }, _, { dataSources }) => {
             return dataSources.trackApi.getTrackModules(id);
         },
     }
