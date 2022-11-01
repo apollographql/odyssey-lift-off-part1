a184d64deda70855f0f9273794d670be7f30ea57
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-01 11:27:09 +0100

    Rendered track-detail on the track page

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 import { Layout, QueryResult } from '../components';
+import TrackDetail from '../components/track-detail';
 
 const GET_TRACK = gql`
     query GetTrack($trackId: ID!) {
@@ -28,7 +29,17 @@ const GET_TRACK = gql`
 
 
 const Track = ({ trackId }) => {
-    return <Layout></Layout>;
+    
+    const { loading, error, data } = useQuery(GET_TRACK, {
+        variables: { trackId },
+    })
+    
+    return <Layout>
+        <QueryResult error={error} loading={loading} data={data} >
+            <TrackDetail track={data?.track} />
+        </QueryResult>
+    </Layout>;
+
 };
 
 export default Track;
