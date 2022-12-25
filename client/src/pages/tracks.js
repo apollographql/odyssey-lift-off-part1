74467b1af0c08b938a269b3210d369fe8ba75676
Melih Gultekin <mmelihgultekin@gmail.com>
2022-12-25 18:06:41 +0100

    Create TracksForHome query string

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+const TRACKS = gql`
+  query TracksForHome {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
