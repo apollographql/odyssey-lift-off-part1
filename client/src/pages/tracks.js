759fd50fbf4c73b3c3839d9be9937454f40e6cdb
Melih Gultekin <mmelihgultekin@gmail.com>
2022-12-25 18:17:28 +0100

    Query endpoint and show simple states

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from '../components';
-import { gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 
 const TRACKS = gql`
   query TracksForHome {
@@ -24,7 +24,13 @@ const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return 'Loading...';
+
+  if (error) return `Error! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
