5fedc9f5954bceb486f2425627423736d21a4c06
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-06-21 12:07:42 -0600

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,4 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import fs from 'fs/promises';
 
 export default defineConfig(() => ({
