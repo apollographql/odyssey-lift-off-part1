95c43d2499b23494b6189b3656ae20337f454fd7
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-06-21 12:00:37 -0600

    update scaffolding

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import fs from 'fs/promises';
+
+export default defineConfig(() => ({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
+}));
