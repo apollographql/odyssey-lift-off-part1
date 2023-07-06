fac4ad65be5b4bc00ab5e06510e73a6fdedf9c16
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-07-06 13:12:37 -0600

    fix tests

--- client/src/utils/test-utils.js
+++ client/src/utils/test-utils.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'
 import '@testing-library/jest-dom/extend-expect';
 import { MockedProvider } from '@apollo/client/testing';
 
@@ -21,6 +22,14 @@ const renderApollo = (
     options
   );
 };
+
+export const renderWithRouterGenerator = (renderer) => (node, options) => {
+  return renderer(<BrowserRouter>{node}</BrowserRouter>, options)
+}
+
+export const renderWithRouter = renderWithRouterGenerator(render)
+
+export const renderApolloWithRouter = renderWithRouterGenerator(renderApollo)
 
 export * from '@testing-library/react';
 export { renderApollo };
