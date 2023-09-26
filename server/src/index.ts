618ba8e501460b7dccb9675b168324752587be22
IsoDevMate <oumaoduor5827@gmail.com>
2023-09-26 07:07:44 +0300

    schema running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,43 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { typeDefs } = require("./schema");
 
+
+const mocks = {
+  Query: {
+    TracksForHome: () => [...new Array(6)],
+  },
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
 //create an instance of the appollo server class and pass the typeDefs
 
+/*we're generating an executable schema from our typeDefs,and instructing Apollo Server to populate every queried schema
+ field with a placeholder value 
+ */
 const startApolloServer=async()=>{
-    const server =new ApolloServer({typeDefs})
+    const server =new ApolloServer({
+      schema:addMocksToSchema({
+        schema:makeExecutableSchema({typeDefs}),
+        mocks,
+      })
+      })
+    
     //The startStandaloneServer function returns a Promise, so we'll await the results of that call,
     //and pull out the url property from the result.
     const {url}= await startStandaloneServer(server)
--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,16 +1,20 @@
-import gql from "graphql-tag";
+const gql=require ("graphql-tag")
 // gql is a tagged template literal, used for wrapping GraphQL strings like the schema definition
 export const typeDefs = gql`
   # Schema definitions go here
 
   type Track {
-    id:ID!
-    Title:String!
-    "a URL to the image for the track's card"
-    Thumbnail:String
-    Length:Int
-    ModulesCount:Int
-    Author:Author!
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main author"
+    author: Author!
+    "The track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
   }
 
   type Author {
@@ -18,4 +22,10 @@ export const typeDefs = gql`
     name: String!
     photo: String
   }
+
+  type Query {
+    "Get tracks array for homepage grid"
+  tracksForHome: [Track!]!
+  }
+
 `;
