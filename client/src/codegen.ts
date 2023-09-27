c4ab4277da288a305009d69c039a8b0b24b86406
IsoDevMate <oumaoduor5827@gmail.com>
2023-09-27 10:33:33 +0300

    defined the query

--- client/src/codegen.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-    
-};
-
-export default config;
--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__/";
 
+const TRACKS=gql(`
+query GetTracks {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      id
+      name
+      photo
+    }
+  }
+}
+`)
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
