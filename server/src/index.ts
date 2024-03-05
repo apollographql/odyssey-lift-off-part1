d4d8f55677144db84a37027d56ef34014162e23a
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-05 23:24:32 +0530

    make mocks

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,29 @@ import { typeDefs } from "./schema";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
+const mocks = {
+    Track: () => ({
+        id: () => "track_01",
+        title: () => "Astro Kitty, Space Explorer",
+        author: () => {
+            return {
+                name: "Grumpy Cat",
+                photo: "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+            };
+        },
+        thumbnail: () =>
+            "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+        length: () => 1210,
+        modulesCount: () => 6,
+    }),
+};
 async function startApolloServer() {
-    const server = new ApolloServer({ typeDefs });
+    const server = new ApolloServer({
+        schema: addMocksToSchema({
+            schema: makeExecutableSchema({ typeDefs }),
+            mocks,
+        }),
+    });
     const { url } = await startStandaloneServer(server);
 
     console.log(`Server is running! 🚀 
