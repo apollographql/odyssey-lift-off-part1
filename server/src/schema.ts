368e74b29d6785127f7249e3b61d736d3f255450
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-05 21:50:51 +0530

    Buiding own schema 🔥🔥🔥

---
+++ server/src/schema.ts
@@ -0,0 +1,26 @@
+import gql from "graphql-tag";
+
+export const defTypes = gql`
+    type Query {
+        "Get Tracks array for Homepage grid "
+        tracksForHome: [Track!]!
+    }
+
+    "type track for all "
+    type Track {
+        title: String!
+        id: ID!
+        author: Author!
+        thumbnail: String
+        length: Int
+        moduleCount: Int
+    }
+
+    "Author is a complete track or module "
+    type Author {
+        "First and last name included"
+        name: String!
+        id: ID!
+        image: String
+    }
+`;
