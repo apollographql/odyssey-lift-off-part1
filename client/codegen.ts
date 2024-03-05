23a5496f1bc5d4d18c8679e765d139aa23142e53
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-06 01:39:42 +0530

    Codegen cli implemented

---
+++ client/codegen.ts
@@ -0,0 +1,17 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+    schema: "http://localhost:4000",
+    documents: ["src/**/*.tsx"],
+    generates: {
+        "./src/__generated__/": {
+            preset: "client",
+            presetConfig: {
+                gqlTagName: "gql",
+            },
+        },
+    },
+    ignoreNoDocuments: true,
+};
+
+export default config;
