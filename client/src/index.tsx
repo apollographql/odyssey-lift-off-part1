8a961300b664b607c18340f1aebe2bb646d4b050
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-06 00:34:16 +0530

    Apollo client setup 🚀

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
 
-const root = createRoot(document.getElementById('root')!);
+const client = new ApolloClient({
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
+});
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <GlobalStyles />
+            <Pages />
+        </ApolloProvider>
+    </React.StrictMode>
 );
