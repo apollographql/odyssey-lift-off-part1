7f8856940015f0204cd14e9e2b08c683522b0375
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-06 01:49:26 +0530

    Define query

--- client/codegen.ts
+++ client/codegen.ts
@@ -11,7 +11,7 @@ const config: CodegenConfig = {
             },
         },
     },
-    ignoreNoDocuments: true,
+    // ignoreNoDocuments: true,
 };
 
 export default config;
--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Layout } from "../components";
-
+import { gql } from "../__generated__/";
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+const TRACKS = gql(`
+      query GetQuery {
+          tracksForHome {
+            title
+            id
+            thumbnail
+            length
+            moduleCount
+          }
+      }
+`);
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+    return <Layout grid> </Layout>;
 };
 
 export default Tracks;
