74b48c5cb19ff421e98cbc15fde316382bdc7328
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2024-03-14 16:18:50 +0530

    graph ql used

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import { Track } from "../__generated__/graphql";
 
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, error, loading } = useQuery(TRACKS);
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  return (
+    <Layout grid>
+      {data?.tracksForHome?.map((track: Track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </Layout>
+  );
 };
 
 export default Tracks;
---
+++ server/src/schema.ts
@@ -0,0 +1,32 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main author"
+    author: Author!
+    "The track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
+
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+`;
