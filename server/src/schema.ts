0c93df4527ec7523766c7d6a271baac81caa5204
Alphhs <106900714+Alphhs@users.noreply.github.com>
2024-05-14 23:12:19 +0800

    first commit

---
+++ server/src/schema.ts
@@ -0,0 +1,23 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+    }
+    "Author of a complete Track or a Module"
+    type Author {
+        id: ID!
+        name: String!
+        photo: String
+    }
+    type Query {
+    "Get tracks array for homepage grid"
+        tracksForHome: [Track!]!
+    }
+`
