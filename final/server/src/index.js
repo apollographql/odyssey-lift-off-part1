de317c9e0a5d8e267f60a93960b918c9395ac795
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-11-08 11:48:46 -0700

    undo double quote

--- final/server/src/index.js
+++ final/server/src/index.js
@@ -1,26 +1,26 @@
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const { addMocksToSchema } = require("@graphql-tools/mock");
-const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { addMocksToSchema } = require('@graphql-tools/mock');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
-const typeDefs = require("./schema");
+const typeDefs = require('./schema');
 
 const mocks = {
   Query: () => ({
     tracksForHome: () => [...new Array(6)],
   }),
   Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
+    id: () => 'track_01',
+    title: () => 'Astro Kitty, Space Explorer',
     author: () => {
       return {
-        name: "Grumpy Cat",
+        name: 'Grumpy Cat',
         photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
       };
     },
     thumbnail: () =>
-      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
+      'https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg',
     length: () => 1210,
     modulesCount: () => 6,
   }),
