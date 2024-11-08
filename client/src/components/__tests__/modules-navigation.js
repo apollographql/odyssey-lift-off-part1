d84e12c58c40e0ace9c7d9a5f3c7edb3dafd6b64
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-11-08 11:44:39 -0700

    undo double quotes

--- client/src/components/__tests__/modules-navigation.js
+++ client/src/components/__tests__/modules-navigation.js
@@ -1,46 +1,46 @@
-import React from "react";
-import { renderWithRouter, cleanup } from "../../utils/test-utils";
-import ModuleNav from "../modules-navigation";
+import React from 'react';
+import { renderWithRouter, cleanup } from '../../utils/test-utils';
+import ModuleNav from '../modules-navigation';
 
 const mockModule = {
-  id: "l_1",
-  title: "The Night Sky",
+  id: 'l_1',
+  title: 'The Night Sky',
   content:
-    "# Et tempus voces tigride remisso fer coimus\n\n## Montibus arbusta detrectas haud\n\n",
+    '# Et tempus voces tigride remisso fer coimus\n\n## Montibus arbusta detrectas haud\n\n',
   thumbnail: null,
-  videoUrl: "https://youtu.be/dlKzlksOUtU",
-  topic: "Cat-stronomy",
+  videoUrl: 'https://youtu.be/dlKzlksOUtU',
+  topic: 'Cat-stronomy',
   length: 164,
 };
 
 const mockParentTrack = {
-  id: "c_0",
-  title: "Cat-stronomy, an introduction",
-  description: "# Pulchra vehi vidit misera sola armenta secabatur\n\n",
+  id: 'c_0',
+  title: 'Cat-stronomy, an introduction',
+  description: '# Pulchra vehi vidit misera sola armenta secabatur\n\n',
   thumbnail:
-    "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
+    'https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg',
   trackLength: 2377,
   modulesCount: 10,
   numberOfViews: 51,
   author: {
-    name: "Henri, le Chat Noir",
+    name: 'Henri, le Chat Noir',
     photo:
-      "https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjExNzA0OH0",
+      'https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjExNzA0OH0',
   },
   modules: [
     {
-      id: "l_0",
-      title: "Exploring Time and Space",
+      id: 'l_0',
+      title: 'Exploring Time and Space',
       length: 258,
     },
   ],
 };
 
-describe("Modules Navigation View", () => {
+describe('Modules Navigation View', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
-  it("renders without error", () => {
+  it('renders without error', () => {
     renderWithRouter(<ModuleNav module={mockModule} track={mockParentTrack} />);
   });
 });
