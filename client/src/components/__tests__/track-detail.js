f32cf9cd6edf1710a27e52b591177b4c17794f7b
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-11-08 11:41:10 -0700

    update photos

--- client/src/components/__tests__/track-detail.js
+++ client/src/components/__tests__/track-detail.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { renderWithRouter, cleanup } from '../../utils/test-utils';
-import TrackDetail from '../track-detail';
+import React from "react";
+import { renderWithRouter, cleanup } from "../../utils/test-utils";
+import TrackDetail from "../track-detail";
 
 const mockTrack = {
   track: {
-    id: 'c_0',
-    title: 'Cat-stronomy, an introduction',
-    description: '# Pulchra vehi vidit misera sola armenta secabatur\n\n',
+    id: "c_0",
+    title: "Cat-stronomy, an introduction",
+    description: "# Pulchra vehi vidit misera sola armenta secabatur\n\n",
     thumbnail:
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
     length: 2377,
     modulesCount: 10,
     numberOfViews: 51,
     author: {
-      name: 'Henri, le Chat Noir',
+      name: "Henri, le Chat Noir",
       photo:
-        'https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjExNzA0OH0',
+        "https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjExNzA0OH0",
     },
     modules: [
       {
-        id: 'l_0',
-        title: 'Exploring Time and Space',
+        id: "l_0",
+        title: "Exploring Time and Space",
         length: 258,
       },
     ],
   },
 };
 
-describe('Module Detail View', () => {
+describe("Module Detail View", () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
-  it('renders without error', () => {
+  it("renders without error", () => {
     renderWithRouter(<TrackDetail {...mockTrack} />);
   });
 });
--- final/client/src/containers/__tests__/track-card.js
+++ final/client/src/containers/__tests__/track-card.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { renderApolloWithRouter, cleanup, waitForElement } from '../../utils/test-utils';
-import TrackCard from '../track-card';
+import React from "react";
+import {
+  renderApolloWithRouter,
+  cleanup,
+  waitForElement,
+} from "../../utils/test-utils";
+import TrackCard from "../track-card";
 
 const mockTrackCardData = {
-  id: 'c_0',
-  title: 'Cat-stronomy, an introduction',
+  id: "c_0",
+  title: "Cat-stronomy, an introduction",
   thumbnail:
-    'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
   length: 2377,
   author: {
-    name: 'Henri, le Chat Noir',
+    name: "Henri, le Chat Noir",
     photo:
-      'https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjExNzA0OH0',
+      "https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjExNzA0OH0",
   },
 };
 
-describe('Track Card', () => {
+describe("Track Card", () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
-  it('renders track Card', async () => {
+  it("renders track Card", async () => {
     const mocks = [];
     const { getByText } = await renderApolloWithRouter(
       <TrackCard track={mockTrackCardData} />,
