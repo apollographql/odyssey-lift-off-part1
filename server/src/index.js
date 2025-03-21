949da0576612ea6139f24d9e2d94ad745ed7d832
Alexandra Bumbar <73651307+alebumbar@users.noreply.github.com>
2025-03-21 11:37:02 +0200

    server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
-//TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("graphql-tools/schema");
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    Server is running!
+    Query at ${url}`);
+}
+startApolloServer();
