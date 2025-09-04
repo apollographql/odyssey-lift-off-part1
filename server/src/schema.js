221146c3e835805b40fab0db442e495b1ba47f8b
Michael <mjs070594@gmail.com>
2025-09-04 14:09:22 -0500

    schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const gql = require('graphql-tag');
 
 const typeDefs = gql`
@@ -32,42 +31,4 @@ const typeDefs = gql`
   }
 `;
 
-=======
-const gql = require("graphql-tag");
-
-
-
-const typeDefs = gql`
-type Query {
-    "Query to get all tracks, paginated"
-    tracksForHome: [Track!]!
-}
-"A track is a group of modules that teaches about a specific topic"
-    type Track {
-        id: ID!
-        "The track's title"
-        title: String!
-        "The track's main author"
-        author: Author!
-        "The track's illustration to display in track card or track page detail"
-        thumbnail: String
-        "The track's approximate length to complete, in seconds"
-        length: Int # length in seconds
-        "The number of modules this track contains"
-        modulesCount: Int
-        "The track's complete description, can be in Markdown format"
-    }
-
-"Author of a complete Track"
-    type Author {
-        id: ID!
-        "Author's name"
-        name: String!
-        "Author's profile picture url"
-        photo: String
-}
-    `;
-
-    
->>>>>>> 1de30a3958c056c799701eaf420fb4022701930a
 module.exports = typeDefs;
