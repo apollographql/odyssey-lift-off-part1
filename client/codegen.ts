95647b832567b9a217e416ddf0e28b8c20e03992
Shuxin Huang <sh4409@columbia.edu>
2025-09-14 19:55:50 -0400

    Finished lift off part 1

---
+++ client/codegen.ts
@@ -0,0 +1,19 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000",
+  documents: ["src/**/*.tsx"],
+  generates: {
+    "./src/__generated__/": {
+      preset: 'client',
+      presetConfig: {
+        gqlTagName: "gql"
+      }
+    },
+    "./src/__generated__/types.ts": {
+      plugins: ["typescript", "typescript-operations"],
+    },
+  },
+};
+
+export default config;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,42 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+const mocks = {
+    Query: () => ({
+        tracksForHome: () => [...new Array(6)],
+    }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
+
+async function startApolloServer() {
+    const server = new ApolloServer({ 
+        schema: addMocksToSchema({
+            schema: makeExecutableSchema({ typeDefs }),
+            mocks,
+        }),
+     });
+    const { url } = await startStandaloneServer(server);
+    console.log(`
+        Server is running! 
+        Query at ${ url }
+    `);
+}
+
+startApolloServer();
