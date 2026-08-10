09bc7d58dff7b9854ac55710c9c429d0d50d420d
Michelle Mabuyo <michelle@apollographql.com>
2026-08-10 12:23:46 -0400

    Move server schema into schema.graphql and load it with readFileSync.
    Aligns Lift-off starters/finals with Odyssey lessons that define SDL in a .graphql file instead of embedding it in JS/TS via gql tags.

--- final/server/src/index.ts
+++ final/server/src/index.ts
@@ -1,8 +1,16 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+import { readFileSync } from "fs";
+import path from "path";
+import gql from "graphql-tag";
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
 
 const mocks = {
   Query: () => ({
